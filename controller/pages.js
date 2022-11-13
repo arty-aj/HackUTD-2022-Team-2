18f42f754fb8868dbbf5a1c90f0a64a11574d204
acoro013 <carturo06@gmail.com>
2022-11-13 10:58:43 -0600

    HTML to ejs

--- controller/pages.js
+++ controller/pages.js
@@ -5,8 +5,20 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.get('/a', (req, res) => {
+router.get('/12', (req, res) => {
     res.render('asteroid12')
+})
+
+router.get('/3', (req, res) => {
+    res.render('asteroids3')
+})
+
+router.get('/8', (req, res) => {
+    res.render('asteroids8')
+})
+
+router.get('/18', (req, res) => {
+    res.render('asteroids18')
 })
 
 module.exports = router;
