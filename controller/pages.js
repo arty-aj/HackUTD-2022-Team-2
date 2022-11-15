0ac1963cd41928221b968bd7790fd9cd54e7a010
seabassr <sebrodriguez1299@icloud.com>
2022-11-15 09:44:35 -0600

    shorted code and made most of the pages dymanic

--- controller/pages.js
+++ controller/pages.js
@@ -5,24 +5,8 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.get('/12', (req, res) => {
-    res.render('asteroid12')
-})
-
-router.get('/3', (req, res) => {
-    res.render('asteroids3')
-})
-
-router.get('/8', (req, res) => {
-    res.render('asteroids8')
-})
-
-router.get('/18', (req, res) => {
-    res.render('asteroids18')
-})
-
-router.get('/15', (req, res) => {
-    res.render('asteroids15')
+router.get('/juypter/:asteroidNum', (req, res) => {
+    res.render('juypter', {page: req.params.asteroidNum});
 })
 
 module.exports = router;
